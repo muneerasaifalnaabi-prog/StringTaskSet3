acbed4d223c746934e1eb3a55842a70dd66a15de
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:00:03 +0400

    Completed task1 countYZ

--- src/countYZ.java
+++ src/countYZ.java
@@ -1,7 +1,9 @@
 public class countYZ {
     public static void main(String[] args) {
             String str="fez day";
+        String str1="day fyyyz";
         System.out.println(countYZ(str));
+        System.out.println(countYZ(str1));
 
     }
     public static Integer countYZ(String str){
@@ -9,7 +11,7 @@ public class countYZ {
         str=str.toLowerCase();
         for (int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            if ((ch=='y'|| ch=='z')&&(i==str.length()-1 || !Character.isLetter(i+1))){
+            if ((ch=='y'|| ch=='z')&&(i==str.length()-1 || !Character.isLetter(str.charAt(i + 1)))){
                 count++;
 
 
