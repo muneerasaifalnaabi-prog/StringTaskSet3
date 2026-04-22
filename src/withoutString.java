cc14eebd792ccc1826b9a27d11d310a5cac4657e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:18:26 +0400

    updated class withoutString

--- src/withoutString.java
+++ src/withoutString.java
@@ -2,5 +2,8 @@ public class withoutString {
     public static void main(String[] args) {
 
     }
-    public static String withoutString(String base,)
+    public static String withoutString(String base,String remove){
+
+    }
+
 }
