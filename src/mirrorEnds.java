13e8af63c6ac444d62b01dc36c3f5957389a1895
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 11:09:43 +0400

    Completed Task 8 mirrorEnds

--- src/mirrorEnds.java
+++ src/mirrorEnds.java
@@ -1,8 +1,18 @@
 public class mirrorEnds {
     public static void main(String[] args) {
+        String str ="xxx";
+        System.out.println(mirrorEnds(str));
 
     }
     public static String mirrorEnds(String str){
-
+        int middle = str.length() / 2;
+        for (int i = middle; i >= 0; i--) {
+            String left = str.substring(0, i);
+            if (str.endsWith(left)) {
+                return left;
+            }
+        }
+        return "";
     }
-}
+    }
+
