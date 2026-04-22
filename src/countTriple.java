3dfa1cdc101ed394abeced2827c2fbfe47399913
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 10:33:54 +0400

    updated class countTriple

--- src/countTriple.java
+++ src/countTriple.java
@@ -1,6 +1,7 @@
 public class countTriple {
     public static void main(String[] args) {
-        String str
+        String str="abcXXXabc";
+        System.out.println(countTriple(str));
 
     }
     public static  Integer countTriple(String str){
