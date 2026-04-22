a8f247dc8fbd21c1e14c1b3364e63e7b5176275c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 11:31:06 +0400

    Completed Task 9 maxBlock

--- src/maxBlock.java
+++ src/maxBlock.java
@@ -13,6 +13,7 @@ public class maxBlock {
         for(int i=0 ; i<str.length();i++){
             if(i<str.length()-1&& str.charAt(i)==str.charAt(i+1) ){
                 count++;
+
                 max=Math.max(max,count);
             }
             else {
