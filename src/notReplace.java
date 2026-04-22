3febb04de56f9210857a4a54379f7055fe063b65
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 13:05:08 +0400

    updated class withoutString

--- src/notReplace.java
+++ src/notReplace.java
@@ -7,7 +7,7 @@ public class notReplace {
         for(int i = 0; i < str.length(); i++){
             if(i-1 >= 0 && Character.isLetter(str.charAt(i-1))
                     || i+2 < str.length() && Character.isLetter(str.charAt(i+2))) {
-                result += str.charAt(i);
+                     result += str.charAt(i);
             }
              else if(i+1 < str.length() && str.substring(i, i+2).equals("is")) {
                 result += "is not";
