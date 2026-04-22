47040cc14f3c86b37fb237af88fb72f769e45beb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 10:18:06 +0400

    updated class gHappy

--- src/gHappy.java
+++ src/gHappy.java
@@ -6,8 +6,9 @@ public class gHappy {
         str=str.toLowerCase();
         for (int i=0;i<str.length();i++){
             char c =str.charAt(i);
-            if (c=='g'){
-                if (c-1='g' || c+1='g'){
+            if (c == 'g') {
+                if ((i > 0 && str.charAt(i - 1) == 'g') ||
+                        (i < str.length() - 1 && str.charAt(i + 1) == 'g')) {
                     return true;
                 }
             }
