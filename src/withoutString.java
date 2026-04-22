5d3e930120b9c0eae02af4f711033ac275cba1be
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:25:23 +0400

    updated class withoutString

--- src/withoutString.java
+++ src/withoutString.java
@@ -5,8 +5,20 @@ public class withoutString {
     public static String withoutString(String base,String remove){
         base=base.toLowerCase();
         remove=remove.toLowerCase();
+        String result ="";
 
+        for (int i=0;i<base.length();i++){
+            if(base.substring(i,i+remove.length()).equals(remove)){
+                i+=remove.length();
+            }
+            else{
+                result+=base.charAt(i);
+                i++;
 
+            }
+        }
+
+return result;
 
 
     }
