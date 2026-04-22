a4b540976762a4fe324c294cb31178df70fb3d85
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 12:32:29 +0400

    Completed Task 11 notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -1,10 +1,12 @@
 public class notReplace {
     public static void main(String[] args) {
-        System.out.println(notReplace("is"));
+        System.out.println(notReplace("is or"));
     }
     public static String notReplace(String str){
         String result = "";
         int len = str.length();
+
+
         for(int i = 0; i < len; i++){
             if(i-1 >= 0 && Character.isLetter(str.charAt(i-1))
                     || i+2 < len && Character.isLetter(str.charAt(i+2))) {
