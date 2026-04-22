24acda8549353a0977062aa966ec33e43ee97182
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 12:37:46 +0400

    Completed Task 11 notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -4,15 +4,12 @@ public class notReplace {
     }
     public static String notReplace(String str){
         String result = "";
-        int len = str.length();
-
-
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < str.length(); i++){
             if(i-1 >= 0 && Character.isLetter(str.charAt(i-1))
-                    || i+2 < len && Character.isLetter(str.charAt(i+2))) {
+                    || i+2 < str.length() && Character.isLetter(str.charAt(i+2))) {
                 result += str.charAt(i);
             }
-             else if(i+1 < len && str.substring(i, i+2).equals("is")) {
+             else if(i+1 < str.length() && str.substring(i, i+2).equals("is")) {
                 result += "is not";
                 i++;
             }
