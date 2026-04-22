129f8bdec9bd1204014277fc9d34f30595d6a5a5
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:13:49 +0400

    Completed Task6 sumDigits

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -1,7 +1,9 @@
 public class sumDigits {
     public static void main(String[] args) {
-String str ="aa1bc2d3";
+        String str ="aa1bc2d3";
+        String str1 ="aa11b33";
         System.out.println(sumDigits(str));
+        System.out.println(sumDigits(str1));
     }
     public static Integer sumDigits(String str){
         int sum=0;
