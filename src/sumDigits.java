1e98e44b2ab0b9d54d64ddd31a411db93b43b774
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:11:10 +0400

    updated class sumDigits

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -1,6 +1,7 @@
 public class sumDigits {
     public static void main(String[] args) {
 String str ="aa1bc2d3";
+        System.out.println(sumDigits(str));
     }
     public static Integer sumDigits(String str){
         int sum=0;
