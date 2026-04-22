c8eeeda0451992ab7dd23b7695bbe4fa2013493d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:43:16 +0400

    updated class withoutString

--- src/withoutString.java
+++ src/withoutString.java
@@ -1,7 +1,7 @@
 public class withoutString {
     public static void main(String[] args) {
             String str ="Hello there";
-            String str1 ="llo";
+            String str1 ="e";
         System.out.println(withoutString(str,str1));
 
     }
@@ -13,9 +13,12 @@ public class withoutString {
         while ((base.contains(remove.toLowerCase()))){
             i=base.indexOf(remove.toLowerCase());
             result=base.substring(i,i+remove.length());
+            base=base.replace(result,"");
+            base=base.toLowerCase();
+
         }
 
-
+return base;
     }
 
 }
