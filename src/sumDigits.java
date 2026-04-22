cb9092f8126d5f53da9a0371e4894283c403163c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:05:58 +0400

    Added class sumDigits

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -1,2 +1,12 @@
 public class sumDigits {
+    public static void main(String[] args) {
+
+    }
+    public static Integer sumDigits(String str){
+        int sum=0;
+        for (int i=0;i<str.length();i++){
+            char c =str.charAt(i);
+            if(Character.isDigit(c))
+        }
+    }
 }
