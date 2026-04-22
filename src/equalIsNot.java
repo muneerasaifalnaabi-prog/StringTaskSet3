5a4bb6f1fc6224daa8e849cb972f0f8daa1841fc
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 09:50:24 +0400

    updated  class equalIsNot

--- src/equalIsNot.java
+++ src/equalIsNot.java
@@ -2,6 +2,15 @@ public class equalIsNot {
     public static void main(String[] args) {
 
     }
-    public static Boolean equalIsNot(String str){}
+    public static Boolean equalIsNot(String str){
+        int numOfIs=0;
+        int numOfNot=0;
+        for (int i=0;i<str.length();i++){
+            if (str.contains("is")){
+                numOfIs++;
+            }
+            if (str.contains())
+        }
+    }
 
 }
