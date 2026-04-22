a16f966a6d8532748fdbc0ef52912ec2ddc73826
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 08:49:13 +0400

    updated  class countYZ

--- src/countYZ.java
+++ src/countYZ.java
@@ -6,7 +6,10 @@ public class countYZ {
         int count=0;
         str=str.toLowerCase();
         for (int i=0;i<str.length();i++){
-            char ch=
+            char ch=str.charAt(i);
+            if ((ch=='y'|| ch=='z')&&(i==str.length()-1 || !Character.isLetter(i+1)){
+
+            }
 
 
         }
