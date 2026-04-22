d5519a96e83b2b12a97ffbfed483efcbef6d25b1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 11:19:28 +0400

    Added class maxBlock

--- src/maxBlock.java
+++ src/maxBlock.java
@@ -1,5 +1,16 @@
 public class maxBlock {
     public static void main(String[] args) {
+        String str ="hoopla";
+        System.out.println(maxBlock(str));
 
+    }
+    public static Integer maxBlock(String str){
+        int count =0;
+        for (int i=0;i<str.length();i++){
+            if(i>0 && str.charAt(i)==i && str.charAt(i-1)==i || str.charAt(i-1)==i && i<str.length()){
+                count++;
+            }
+        }
+        return count;
     }
 }
