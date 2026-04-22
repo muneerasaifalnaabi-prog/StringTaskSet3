97c5bf1a39747e6b05cf9d2467266258b392286c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-22 10:51:53 +0400

    Updated  class sameEnds

--- src/sameEnds.java
+++ src/sameEnds.java
@@ -1,5 +1,7 @@
 public class sameEnds {
     public static void main(String[] args) {
+        String str = "abXYab";
+        System.out.println(sameEnds(str));
 
     }
 
@@ -7,7 +9,7 @@ public class sameEnds {
         String result = "";
         for (int i=0;i<str.length()/2;i++){
             String start =str.substring(0,i);
-            String end =str.substring(str.length()-1);
+            String end =str.substring(str.length()-i);
             if (start.equals(end)) {
                 result = start;
             }
